8691b61bca53c02b92fed9a959af7ac24bff79c8
luisquintanilla <Luis.Quintanilla@microsoft.com>
2020-05-28 00:11:08 -0400

    End Phase 2.1

--- src/TrainConsole/Program.cs
+++ src/TrainConsole/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using Microsoft.ML;
+using Shared;
 
 namespace TrainConsole
 {
     class Program
     {
+        private static string TRAIN_DATA_FILEPATH = @"C:\Dev\true_car_listings.csv";
+
         static void Main(string[] args)
         {
             MLContext mlContext = new MLContext();
+
+            // Load training data
+            Console.WriteLine("Loading data...");
+            IDataView trainingData = mlContext.Data.LoadFromTextFile<ModelInput>(path: TRAIN_DATA_FILEPATH, hasHeader: true, separatorChar: ',');
         }
     }
 }
