e4f89f2460436cfd43ee0bfbe4af56f09f2a604b
Alexander Slotte <alexander.slotte@gmail.com>
2020-07-18 15:51:41 -0400

    Removed datatests

--- test/DataTests/DataValidationTests.cs
+++ test/DataTests/DataValidationTests.cs
@@ -11,49 +11,31 @@ namespace DataTests
     [TestClass]
     public class DataValidationTests
     {
-        private static string TRAIN_DATA_FILEPATH = @"/media/data/true_car_listings.csv";
-
-        private static IEnumerable<ModelInput> Rows;
+        private static string TRAIN_DATA_FILEPATH = "";
 
         [ClassInitialize]
         public static void Initialize(TestContext testContext)
         {
-            var mlContext = new MLContext();
-            var data = mlContext.Data.LoadFromTextFile<ModelInput>(TRAIN_DATA_FILEPATH, hasHeader: true, separatorChar: ',');
-
-            Rows = mlContext.Data.CreateEnumerable<ModelInput>(data, false);
         }
 
         [TestMethod]
         public void VerifyValidMilage()
         {
-            var hasInvalidMilage = Rows.Any(x => x.Mileage < 0);
-
-            hasInvalidMilage.Should().BeFalse();
         }
 
         [TestMethod]
         public void VerifyValidPrice()
         {
-            var hasNegativePrice = Rows.Any(x => x.Price < 0);
-
-            hasNegativePrice.Should().BeFalse();
         }
 
         [TestMethod]
         public void VerifyValidYear()
         {
-            var hasValidYears = Rows.All(x => x.Year > 1950 && x.Year < DateTime.Now.Year + 1);
-
-            hasValidYears.Should().BeTrue();
         }
 
         [TestMethod]
         public void VerifyMinimumNumberOfRows()
         {
-            var rowCount = Rows.Count();
-
-            rowCount.Should().BeGreaterThan(10000);
         }
     }
 }
