6aebe1dfd6ff899e5331ac5513e86b5e6ad6beb7
luisquintanilla <Luis.Quintanilla@microsoft.com>
2020-07-28 22:49:52 -0400

    Updated to latest version of ap

---
+++ test/DataTests/DataValidationTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.ML;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Shared;
+using System.Linq;
+using FluentAssertions;
+
+namespace DataTests
+{
+    [TestClass]
+    public class DataValidationTests
+    {
+        private static string TRAIN_DATA_FILEPATH = "";
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+
+        }
+
+        [TestMethod]
+        public void VerifyValidMilage()
+        {
+
+        }
+
+        [TestMethod]
+        public void VerifyValidPrice()
+        {
+
+        }
+
+        [TestMethod]
+        public void VerifyValidYear()
+        {
+
+        }
+
+        [TestMethod]
+        public void VerifyMinimumNumberOfRows()
+        {
+
+        }
+    }
+}
---
+++ test/ModelTests/ModelTests.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+using Microsoft.ML;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void Given_LowRangeCar_ShouldEstimatePriceWithinRange()
+        {
+
+        }
+
+        [TestMethod]
+        public void Given_MidRangeCar_ShouldEstimatePriceWithinRange()
+        {
+
+        }
+
+        [TestMethod]
+        public void Given_HighRangeCar_ShouldEstimatePriceWithinRange()
+        {
+
+        }
+    }
+}
